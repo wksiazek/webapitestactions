7e890f5b1d95ab8ba7586a90fc5baa737dc323a2
Wojciech Książek <wojtekksiazekk@gmail.com>
2025-05-01 16:58:50 +0200

    zmiany

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -29,4 +29,16 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+    [HttpGet(Name = "GetWeatherForecast2")]
+    public IEnumerable<WeatherForecast> Get2()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
 }
